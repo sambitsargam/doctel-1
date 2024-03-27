1ef611b6c94792686c50e3dc7f62181b27332f8c
v charan raju <96484862+VCharanRaju12@users.noreply.github.com>
2024-03-27 16:57:47 +0530

    Update HomeComponent.js

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -4,9 +4,11 @@ import "../App.css";
 function Home(props) {
   return (
     <div className="container">
+    <div className="center">
       <i className="fa fa-user-md fa-5x"></i>
       <h2>Welcome to DocTel Application</h2>
       <br />
+    </div>
     </div>
   );
 }
